1a9ebe6a5a750641165cbde94233f435b7f53f10
eagerterrier <eagerterrier@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-03 16:13:57 +0000

    Adding version 2.0.2 as tag
    git-svn-id: https://plugins.svn.wordpress.org/mimetypes-link-icons/tags/2.0.2@418845 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/mtli_str_replace.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+	content = jQuery('#content').html();
+	replace_content = 0;
+	for(mime_type in mtli_js_array){
+		string_match = new RegExp('href="([^"#]+\\.'+mtli_js_array[mime_type]+')(#?[^" ]*)"','gi');
+		if(content.match(string_match)&&mtli_js_array[mime_type]=='pdf'){
+			content = content.replace(string_match,'href="$1$2"  class="mtli_attachment mtli_'+mtli_js_array[mime_type]+'"');
+			replace_content=1;
+		}
+	}
+	if(replace_content==1){
+		jQuery('#content').html(content);
+	}
+});
