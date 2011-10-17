f22c5c2800739e66b501d4f767776e37c79c0b39
eagerterrier <eagerterrier@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-17 05:29:30 +0000

    Adding tags for 2.0.9
    git-svn-id: https://plugins.svn.wordpress.org/mimetypes-link-icons/tags/2.0.9@451848 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/mtli_str_replace.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($) {
+	if(jQuery('#content').length){
+		content = jQuery('#content').html();
+	} else {
+		content = jQuery('.status-publish:first').parent().html();
+	}
+	replace_content = 0;
+	for(mime_type in mtli_js_array){
+		string_match = new RegExp('href="([^"#]+\\.'+mtli_js_array[mime_type]+')(#[^" ]+"|")','gi');
+		if(content.match(string_match)){
+			content = content.replace(string_match,'href="$1$2  class="mtli_attachment mtli_'+mtli_js_array[mime_type]+'"');
+			replace_content=1;
+		}
+	}
+	if(replace_content==1){
+		if(jQuery('#content').length){
+			jQuery('#content').html(content);
+		} else {
+			jQuery('.status-publish:first').parent().html(content);
+		}
+		if(mtli_hidethings){
+			jQuery('.mtli_attachment').each(function(){
+				if($(this).parents('.'+mtli_avoid).length){
+					$(this).removeClass('mtli_attachment').css('background-image','none');
+				}
+			});
+		}
+	}
+	
+});
