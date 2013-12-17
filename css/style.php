b5e27b3e4a7910f55301a43143481fc76d8a6c72
eagerterrier <eagerterrier@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-17 07:03:26 +0000

    v3.2
    git-svn-id: https://plugins.svn.wordpress.org/mimetypes-link-icons/trunk@823700 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- css/style.php
+++ css/style.php
@@ -1,23 +1,24 @@
 <?php
-header('Content-type: text/css');
+header( 'Content-type: text/css' );
 
 $getvars = explode( '&', base64_decode( $_GET['cssvars'] ) );
-foreach( $getvars as $v ) {
-	$data = explode( '=', $v );
+foreach ( $getvars as $v ) {
+	$data     = explode( '=', $v );
 	$$data[0] = $data[1];
 }
 unset( $getvars, $v );
 
 /* Input validation of $_GET variables */
-$mtli_height = ( in_array( intval( $mtli_height ), array( 16, 24, 48, 64, 128 ) ) ? intval( $mtli_height ) : 16 );
+$mtli_height      = ( in_array( intval( $mtli_height ), array( 16, 24, 48, 64, 128 ) ) ? intval( $mtli_height ) : 16 );
 $mtli_leftorright = ( in_array( $mtli_leftorright, array( 'left', 'right' ) ) ? $mtli_leftorright : 'left' );
-$mtli_image_type = ( in_array( $mtli_image_type, array( 'gif', 'png' ) ) ? $mtli_image_type : 'png' );
+$mtli_image_type  = ( in_array( $mtli_image_type, array( 'gif', 'png' ) ) ? $mtli_image_type : 'png' );
 
 
 echo '
 .mtli_attachment {
 	display:inline-block;
-	height:' . $mtli_height . 'px;
+	height: auto;
+	min-height:' . $mtli_height . 'px;
 	background-position: top ' . $mtli_leftorright . ';
 	background-attachment: scroll;
 	background-repeat: no-repeat;
@@ -28,20 +29,20 @@ $mtli_available_mime_types = array(
 			'3g2', '3gp',
 			'ai', 'air', 'asf', 'avi',
 			'bib',
-			'csv',
+			'cls', 'csv',
 			'deb', 'djvu', 'dmg', 'doc', 'docx', 'dwf', 'dwg',
 			'eps', 'epub', 'exe',
-			'flac', 'flv',
+			'f', 'f77', 'f90', 'flac', 'flv',
 			'gif', 'gz',
 			'ico', 'indd', 'iso',
 			'jpg', 'jpeg',
 			'log',
 			'm4a', 'm4v', 'midi', 'mkv', 'mov', 'mp3', 'mp4', 'mpeg', 'mpg', 'msi',
 			'odp', 'ods', 'odt', 'oga', 'ogg', 'ogv',
-			'pdf', 'png', 'pps', 'ppsx', 'ppt', 'pptx', 'psd', 'pub',
+			'pdf', 'png', 'pps', 'ppsx', 'ppt', 'pptx', 'psd', 'pub', 'py',
 			'qt',
-			'ra', 'ram', 'rm', 'rpm', 'rtf', 'rv',
-			'skp', 'spx', 'sql',
+			'ra', 'ram', 'rar', 'rm', 'rpm', 'rtf', 'rv',
+			'skp', 'spx', 'sql', 'sty',
 			'tar', 'tex', 'tgz', 'tiff', 'ttf', 'txt',
 			'vob',
 			'wav', 'wmv',
@@ -50,7 +51,7 @@ $mtli_available_mime_types = array(
 );
 
 // @todo Only generate for the enabled mime-types ?
-foreach( $mtli_available_mime_types as $type ) {
+foreach ( $mtli_available_mime_types as $type ) {
 	echo '
 .mtli_' . $type . ' {
 	background-image: url(../images/' . $type . '-icon-' . $mtli_height . 'x' . $mtli_height . '.' . $mtli_image_type . '); }';
